d754ae056e01bdac3dc0802240ceff4caea1ecd9
-help <federicopozzoni95@gmail.com>
2020-10-30 11:05:37 +0100

    Step 4: XML Views

--- webapp/index.js
+++ webapp/index.js
@@ -5,6 +5,8 @@ sap.ui.define([
     "use strict";
 
     XMLView.create({
-        viewName:sap.ui.demo.walkthrough.view.App
-    })
+        viewName: "sap.ui.demo.walkthrough.view.App"
+    }).then( function(oView){
+        oView.placeAt("content");
+    });
 });
